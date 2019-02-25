3d758366d22ce5af7d3114697b6231c6ebea5468
Manish Agrawal <manish.agrawal@gmail.com>
2019-02-25 08:16:37 -0500

    Added chart.js example

--- MVC_Start/Controllers/HomeController.cs
+++ MVC_Start/Controllers/HomeController.cs
@@ -18,5 +18,30 @@ namespace MVC_Start.Controllers
     {
       return View();
     }
+
+    /// <summary>
+    /// Replicate the chart example in the JavaScript presentation
+    /// 
+    /// Typically LINQ and SQL return data as collections.
+    /// Hence we start the example by creating collections representing the x-axis labels and the y-axis values
+    /// However, chart.js expects data as a string, not as a collection.
+    ///   Hence we join the elements in the collections into strings in the view model
+    /// </summary>
+    /// <returns>View that will display the chart</returns>
+    public ViewResult DemoChart()
+    {
+      string[] ChartLabels = new string[] { "Africa", "Asia", "Europe", "Latin America", "North America" };
+      int[] ChartData = new int[] { 2478, 5267, 734, 784, 433 };
+
+      ChartModel Model = new ChartModel
+      {
+        ChartType = "bar",
+        Labels = String.Join(",", ChartLabels.Select(d => "'" + d + "'")),
+        Data = String.Join(",", ChartData.Select(d => d)),
+        Title = "Predicted world population (millions) in 2050"
+      };
+
+      return View(Model);
+    }
   }
 }
--- MVC_Start/Models/App_Models.cs
+++ MVC_Start/Models/App_Models.cs
@@ -11,4 +11,12 @@ namespace MVC_Start.Models
     public string Email { get; set; }
     public string Phone { get; set; }
   }
+
+  public class ChartModel
+  {
+    public string ChartType { get; set; }
+    public string Labels { get; set; }
+    public string Data { get; set; }
+    public string Title { get; set; }
+  }
 }
